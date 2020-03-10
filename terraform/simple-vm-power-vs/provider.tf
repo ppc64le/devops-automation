8a18d70614713caab9f66a8339c1d82bffa81fae
Joe Cropper <jwcroppe@us.ibm.com>
2020-03-10 08:56:52 -0500

    Add support for multi-zone regions

--- terraform/simple-vm-power-vs/provider.tf
+++ terraform/simple-vm-power-vs/provider.tf
@@ -2,4 +2,5 @@ provider "ibm" {
     version          = ">= 0.18.0"
     ibmcloud_api_key = "${var.ibmcloud_api_key}"
     region           = "${var.ibmcloud_region}"
+    #zone             = "${var.ibmcloud_zone}"
 }
