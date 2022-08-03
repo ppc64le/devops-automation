4ed2259f8b536d3b50b58592c0a17a83ae04e1fb
Joe Cropper <jwcroppe@us.ibm.com>
2022-08-03 11:19:59 -0500

    Update provider.tf
    Updated to work with latest IBM Cloud Terraform provider (1.44.0 at the time of this writing).

--- terraform/simple-vm-power-vs/provider.tf
+++ terraform/simple-vm-power-vs/provider.tf
@@ -1,6 +1,15 @@
+terraform {
+    required_version = ">=1.0.0, <2.0"
+    required_providers {
+        ibm = {
+            source  = "IBM-Cloud/ibm"
+            version = ">= 1.44.0"
+        }
+    }
+}
+
 provider "ibm" {
-    version          = ">= 0.18.0"
-    ibmcloud_api_key = "${var.ibmcloud_api_key}"
-    region           = "${var.ibmcloud_region}"
-    #zone             = "${var.ibmcloud_zone}"
+    ibmcloud_api_key = var.ibmcloud_api_key
+    region           = var.ibmcloud_region
+    zone             = var.ibmcloud_zone
 }
