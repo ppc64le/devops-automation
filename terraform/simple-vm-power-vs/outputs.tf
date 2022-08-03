09ba16597cd67b7e80dbf49addd2e949650e6c2e
Joe Cropper <jwcroppe@us.ibm.com>
2022-08-03 11:14:58 -0500

    Update outputs.tf

--- terraform/simple-vm-power-vs/outputs.tf
+++ terraform/simple-vm-power-vs/outputs.tf
@@ -1,19 +1,19 @@
 output "status" {
-  value = ibm_pi_instance.pvminstance.status
+    value = ibm_pi_instance.pvminstance.status
 }
 
 output "processors" {
-  value = ibm_pi_instance.pvminstance.pi_processors
+    value = ibm_pi_instance.pvminstance.pi_processors
 }
 
 output "health_status" {
-  value = ibm_pi_instance.pvminstance.health_status
+    value = ibm_pi_instance.pvminstance.health_status
 }
 
 output "network" {
-  value = ibm_pi_instance.pvminstance.pi_network
+    value = ibm_pi_instance.pvminstance.pi_network
 }
 
 output "progress" {
-  value = ibm_pi_instance.pvminstance.pi_progress
+    value = ibm_pi_instance.pvminstance.pi_progress
 }
